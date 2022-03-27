9790f88fc541c1bc1b88f220de5207be17b5a620
Michael Pratt <mcpratt@pm.me>
2022-03-27 16:00:42 -0400

    Revert "kernel: remove support for kernel 4.19"
    This reverts commit 278512665094888d3c007fdd74e090496d6c811d.
    
    add support for 4.19 back
    
    makes it default

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -10,6 +10,10 @@ LINUX_VERSION-5.4 = .182
 
 LINUX_KERNEL_HASH-5.4.182 = b2f1201f64f010e9e3c85d6f303a559a7944a80a0244a86b8f5035bd23f1f40d
 
+LINUX_VERSION-4.19 = .138
+
+LINUX_KERNEL_HASH-4.19.138 = d15c27d05f6c527269b75b30cc72972748e55720e7e00ad8abbaa4fe3b1d5e02
+
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
 
