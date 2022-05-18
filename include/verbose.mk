1f2d9936a09bc981263e895d5759628d37fa1007
Michael Pratt <mcpratt@pm.me>
2022-05-17 23:06:36 -0400

    verbose.mk: fix bad file descriptor error
    Signed-off-by: Michael Pratt <mcpratt@pm.me>

--- include/verbose.mk
+++ include/verbose.mk
@@ -30,12 +30,12 @@ ifeq ($(IS_TTY),1)
 endif
 
 define ERROR_MESSAGE
-  printf "$(_R)%s$(_N)\n" "$(1)" >&8
+  printf "$(_R)%s$(_N)\n" "$(1)" >&2
 endef
 
 ifeq ($(findstring s,$(OPENWRT_VERBOSE)),)
   define MESSAGE
-	printf "$(_Y)%s$(_N)\n" "$(1)" >&8
+	printf "$(_Y)%s$(_N)\n" "$(1)" >&2
   endef
 
   ifeq ($(QUIET),1)
@@ -52,6 +52,12 @@ ifeq ($(findstring s,$(OPENWRT_VERBOSE)),)
     SILENT:=>/dev/null $(if $(findstring w,$(OPENWRT_VERBOSE)),,2>&1)
     export QUIET:=1
     SUBMAKE=cmd() { $(SILENT) $(MAKE) -s "$$@" < /dev/null || { echo "make $$*: build failed. Please re-run make with -j1 V=s or V=sc for a higher verbosity level to see what's going on"; false; } } 8>&1 9>&2; cmd
+    define MESSAGE
+	printf "$(_Y)%s$(_N)\n" "$(1)" >&9
+    endef
+    define ERROR_MESSAGE
+	printf "$(_R)%s$(_N)\n" "$(1)" >&9
+    endef
   endif
 
   .SILENT: $(MAKECMDGOALS)
