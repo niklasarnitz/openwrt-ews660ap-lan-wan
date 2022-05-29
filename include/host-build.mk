76b5cc5eb95356e618b8b566b4ae759e8c57ee15
Michael Pratt <mcpratt@pm.me>
2022-05-29 01:09:38 -0400

    build: do not allow Werror flag in packages
    Signed-off-by: Michael Pratt <mcpratt@pm.me>

--- include/host-build.mk
+++ include/host-build.mk
@@ -87,6 +87,7 @@ define Host/Configure/Default
 		if [ -x $(HOST_CONFIGURE_CMD) ]; then \
 			$(call replace_script,$(HOST_BUILD_DIR)/$(HOST_CONFIGURE_PATH)$(if $(3),/$(strip $(3))),config.guess,$(SCRIPT_DIR)) \
 			$(call replace_script,$(HOST_BUILD_DIR)/$(HOST_CONFIGURE_PATH)$(if $(3),/$(strip $(3))),config.sub,$(SCRIPT_DIR)) \
+			$(call replace_string,$(HOST_BUILD_DIR)/$(HOST_CONFIGURE_PATH)$(if $(3),/$(strip $(3))),'Makefile*',-Werror ,-Wextra ) \
 			$(if $(HOST_ARCH_GNU), \
 				echo "echo $(GNU_HOST_NAME)" > \
 					$(HOST_BUILD_DIR)$(if $(3),/$(strip $(3)))/config.guess ; \
