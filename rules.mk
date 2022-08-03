8b85619121671982ae431badce6e7cb9eae26828
Michael Pratt <mcpratt@pm.me>
2022-08-03 13:01:51 -0400

    package: organize download of raw files
    Some packages download a single raw file or set of raw files
    instead of a zipped archive.
    
    Many of these files have names and version numbers
    that are not directly related to the name and version of the package
    as it is defined in its Makefile.
    
    To better organize downloaded files, this adds the optional function of
    creating a sub-directory named using PKG_NAME and PKG_VERSION
    for these files in the download directory.
    
    In the Openwrt core repo, this applies to drivers and firmware blobs.
    
    Signed-off-by: Michael Pratt <mcpratt@pm.me>

--- rules.mk
+++ rules.mk
@@ -109,7 +109,7 @@ $(foreach t,$(DEFAULT_SUBDIR_TARGETS) $(1),
 )
 endef
 
-DL_DIR:=$(if $(call qstrip,$(CONFIG_DOWNLOAD_FOLDER)),$(call qstrip,$(CONFIG_DOWNLOAD_FOLDER)),$(TOPDIR)/dl)
+DL_DIR:=$(if $(call qstrip,$(CONFIG_DOWNLOAD_FOLDER)),$(call qstrip,$(CONFIG_DOWNLOAD_FOLDER)),$(TOPDIR)/dl)$(if $(DL_MKDIR),/$(PKG_NAME)$(if $(PKG_VERSION),-$(PKG_VERSION)))
 OUTPUT_DIR:=$(if $(call qstrip,$(CONFIG_BINARY_FOLDER)),$(call qstrip,$(CONFIG_BINARY_FOLDER)),$(TOPDIR)/bin)
 BIN_DIR:=$(OUTPUT_DIR)/targets/$(BOARD)/$(SUBTARGET)
 INCLUDE_DIR:=$(TOPDIR)/include
